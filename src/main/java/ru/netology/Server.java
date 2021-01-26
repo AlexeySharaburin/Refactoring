d4a962a22730511b1695d8221c436cd4ed330841
Alexey Sharaburin <alexey.sharaburin@gmail.com>
2021-01-26 14:48:57 +0100

    Add Handlers

--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -1,24 +1,44 @@
 package ru.netology;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 
 public class Server {
 
     private final int portNumber;
+    private final Map<String, Map<String, Handler>> handlers;
+
+
+
+//    private final Handler notFoundHandler = (request, out) -> {
+//        try {
+//            out.write((
+//                    "HTTP/1.1 404 Not Found\r\n" +
+//                            "Content-Length: 0\r\n" +
+//                            "Connection: close\r\n" +
+//                            "\r\n"
+//            ).getBytes());
+//            out.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    };
 
     public Server(int portNumber) {
         this.portNumber = portNumber;
+        handlers = new ConcurrentHashMap<>();
     }
-
 
     public void connectServer() throws IOException {
         final var serverSocket = new ServerSocket(portNumber);
         final var threadPool = Executors.newFixedThreadPool(64);
-        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png");
+        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
         System.out.println("Сервер начал работать!");
 
@@ -26,7 +46,7 @@ public class Server {
             while (true) {
                 Socket socket = serverSocket.accept();
                 try {
-                    final var serverThread = new ServerThread(socket, validPaths);
+                    final var serverThread = new ServerThread(socket, validPaths, handlers);
                     threadPool.submit(serverThread);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -38,5 +58,12 @@ public class Server {
 
         threadPool.shutdown();
     }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Optional.ofNullable(handlers.get(method))
+                .ifPresentOrElse(pathHandlerMap -> pathHandlerMap.put(path, handler),
+                        () -> handlers.put(method, new ConcurrentHashMap<>(Map.of(path, handler))));
+    }
+
 }
 
